f3de1af7da3f8b39f79ffee82cb51155b136b4ac
Harut Aghayan <agayanharut1@gmail.com>
2019-10-28 13:59:45 +0400

    001

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import "./style.scss";
-// import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
-
-// import {Home} from "./components/Home";
-// import About from "./components/about";
-// import Contact from "./components/contact";
-// import logo from "./img/ford-logo.png";
 
 function App() {
   let links = ["home","about","contact us","sign in"];
@@ -36,60 +30,4 @@ function NavbarLink({ name }) {
 
 
 
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  /* <BrowserRouter>
-          <div className="nav">
-            <div className="navParentLogo">
-                <img src={logo} class="navLogo"/>
-            </div>
-            <div className="navMeunu">
-                <div><Link to="/">Home</Link></div>
-                <div><Link to="/about">About</Link></div>
-                <div><Link to="/contact">Contact</Link></div>
-                <div><Link to="/example">Example</Link></div>
-            </div>
-          </div>
-
-          <Switch>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/about' component={About} />
-            <Route exact path='/contact' component={Contact} />
-
-            <Route exact path='/example' render={() => {
-              return (
-                <div>example</div>
-              );
-            }} />
-          </Switch>
-        </BrowserRouter> */

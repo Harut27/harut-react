52ced1b28971a2e02915a8c5c0ae6068143b75e9
Harut Aghayan <agayanharut1@gmail.com>
2019-11-11 18:58:02 +0400

    .

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,24 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 
 const SideBar = props => {
 
     const sidebarMenu = ['test 1','test 2','test 3','test 4','test 5','test 6'];
-    return(
+    return (
         
-            <div className="sitebarColumn">
-                {
-                    sidebarMenu.map((sitebarLink,index)=>{
-                        return(
-                            <div className={`sitebar-link sitebar-${sitebarLink.replace(/\s/g, '')}`} key={index}>{sitebarLink.toUpperCase()}</div>
-                        )
-                    })
-                }
-            </div>
+        <div className="sitebarColumn">
+            {
+                sidebarMenu.map((sitebarLink,index)=>{
+                    return(
+                        <div className={`sitebar-link sitebar-${sitebarLink.replace(/\s/g, '')}`} key={index}>
+                            <Link to={}>
+                                {sitebarLink.toUpperCase()}
+                            </Link>
+                        </div>
+                    )
+                })
+            }
+        </div>
         
     )
 }
-
-
 
 
 export default SideBar;

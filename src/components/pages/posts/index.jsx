b566f55f671ea4e3e87e7f92f87c0a503d8b7a21
Harut Aghayan <agayanharut1@gmail.com>
2019-11-12 19:22:10 +0400

    get data

--- src/components/pages/posts/index.jsx
+++ src/components/pages/posts/index.jsx
@@ -1,67 +1,173 @@
 import React, { Component } from "react";
 import axios from "axios";
+import _ from "lodash";
+
+
+
 class Posts extends Component {
-  constructor(props) {
-    super(props);
+  constructor(props){
+    super(props)
     this.state = {
       count: 0
-    };
+    }
   }
-  // LIFECYCLES
-  componentDidMount() {
-    //  WORKS WHEN COMPONENT IS READY TO USE
-    console.log("component is mounted");
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
-      console.log(res.data, "res");
+
+  componentDidMount(){
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+    .then(res => {
+
+      let users = res.data;
+        console.log(users)
+
+        users.forEach((user,id) => {
+
+          console.log(user)
+          return(
+            <div className="user-cart">
+              <span>
+                {user.ttle}
+              </span>
+              <span>
+                User {user.id}
+              </span>
+              <span>
+                {user.body}
+              </span>
+
+            </div>
+          )
+        });
     });
-    // window.addEventListener("click", this.sayWow);
+    
   }
 
-  componentWillUpdate() {
-    console.log("component will be updated");
-  }
-  componentDidUpdate() {
-    //  WORKS WHEN STATE OR PROPS IS CHANGED
-    console.log("component is updated");
+  handleCountChanage = ()=>{
+    this.setState({
+      count: this.state.count + 1
+    })
   }
 
-  componentWillUnmount() {
-    // WORKS WHEN COMPONENT IS DEAD
-    console.log("component is dead");
-    // window.removeEventListener("click", this.sayWow);
-  }
+  render(){
 
-  componentWillReceiveProps(nextProps) {
-    console.log(nextProps, this.props);
-  }
-
-  sayWow = () => {
-    alert("wow");
-  };
-
-  handleCountChanage = () => {
-    this.setState(
-      {
-        // stipuma noric rend lini u poxi state @  ( ev kara noric rend lini erb ira papai props@ poxvi)
-        count: this.state.count + 1
-      },
-      () => {
-        // CALLBACK FUNCTION FOR GETTING CHANGED STATE VALUE
-        console.log(this.state.count);
-      }
-    );
-  };
-
-  render() {
-    const { count } = this.state;
-    return (
+    const {count} = this.state; 
+    return(
       <div>
         <span>{count}</span>
-
         <button onClick={this.handleCountChanage}>Click</button>
       </div>
-    );
+    )
   }
+
+
+
 }
 
+
 export default Posts;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Posts extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       count: 0
+//     };
+//   }
+
+//   // LIFECYCLES
+    
+//   componentDidMount() {               //  WORKS WHEN COMPONENT IS READY TO USE
+//     console.log("component is mounted");
+//     axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+//       console.log(res.data, "res");
+//     });
+//   }
+//   componentWillUpdate() {
+//     console.log("component will be updated");
+//   }
+//   componentDidUpdate() {  //  WORKS WHEN STATE OR PROPS IS CHANGED
+//     console.log("component is updated");
+//   }
+
+//   componentWillUnmount() {   // WORKS WHEN COMPONENT IS DEAD
+//     console.log("component is dead");
+//     window.addEventListener("click", this.sayWow);
+
+//     // window.removeEventListener("click", this.sayWow);
+//   }
+
+//   componentWillReceiveProps(nextProps) {
+//     console.log(nextProps, this.props);
+//   }
+
+//   sayWow = () => {
+//     // alert("Do You want leave this page?");
+//   };
+
+//   handleCountChanage = () => {
+//     this.setState(     // stipuma noric rend lini u poxi state @  ( ev kara noric rend lini erb ira papai props@ poxvi)
+//       {
+//         count: this.state.count + 1
+//       },
+//       () => {
+//         // CALLBACK FUNCTION FOR GETTING CHANGED STATE VALUE
+//         console.log(this.state.count);
+//       }
+//     );
+//   };
+
+//   render() {
+//     const { count } = this.state;
+//     return (
+//       <div>
+//         <span>{count}</span>
+
+//         <button onClick={this.handleCountChanage}>Click</button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Posts;
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -7,9 +7,9 @@ const SideBar = props => {
     "test 2",
     "test 3",
     "test 4",
-    "test 5",
-    "test 6"
+    "test 5"
   ];
+  
   return (
     <div className="sitebarColumn">
       {sidebarMenu.map((sitebarLink, index) => {

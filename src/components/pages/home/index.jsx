1343f391c6335f7b8a08c70025f9089955c1ccdb
Harut Aghayan <agayanharut1@gmail.com>
2019-11-13 19:03:00 +0400

    home page

--- src/components/pages/home/index.jsx
+++ src/components/pages/home/index.jsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React,{Component} from "react";
+import Axios from "axios";
 
-const Home = props => {
-  return <section>HOME</section>;
-};
+
+
+class Home extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      userInfos: []
+    }
+  }
+
+
+
+  componentDidMount(){
+    Axios.get("https://jsonplaceholder.typicode.com/users")
+    .then(res=>{
+        let userInfos = res.data;
+        this.setState({userInfos})
+    })
+  }
+
+  render(){
+    const { userInfos } = this.state;
+    
+    return(
+      <div className="userInfoContainer">
+        {
+          userInfos.map((userInfo,id)=>{
+            return (
+              <div className="userInfoCart" kay={id}>
+                <span>
+                  <strong>User Name:</strong>  {userInfo.name}
+                </span>
+                <span>
+                  <strong>Phone:</strong>     {userInfo.phone}
+                </span>
+                <span>
+                  <strong>Email:</strong>   {userInfo.email}
+                </span>
+                <span>
+                  <strong>Website:</strong>   {userInfo.website}
+                </span>
+                <span>
+                  <strong>Company Name:</strong>  {userInfo.company["name"]}
+                </span>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+
+}
+
+
+
+// const Home = props => {
+//   return <section>HOME</section>;
+// };
 
 export default Home;
